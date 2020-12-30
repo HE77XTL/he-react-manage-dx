00037e823167e0e67d539618de84e9d71d8d4be6
何东升 <he77505357@163.com>
2020-12-30 17:02:17 +0800

    添加react-app-rewired,cross-env

--- src/App.js
+++ src/App.js
@@ -2,24 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const env = process.env.REACT_APP_ENV;
+    console.log(env)
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>环境变量</div>
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
