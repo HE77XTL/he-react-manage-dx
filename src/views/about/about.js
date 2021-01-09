204353806ca7533311df527148155c2796a9cd9a
何东升 <he77505357@163.com>
2021-01-09 17:39:40 +0800

    lesss 测试

--- src/views/about/about.js
+++ src/views/about/about.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Button } from 'caihrc';
 
+import styles from './about.module.less'
+
 const About  = function () {
     return(
         <div className='dsContent'>
-            <div >
+            <div className={styles.aboutTest}>
                 About
                 <Button>dx-btn</Button>
+                <div className={styles.hhh}>
+                    8888
+                </div>
             </div>
         </div>
     )
